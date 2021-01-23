ddfb7f81f8f1d1e05452f6b66c3f89fc7b17c191
Qunqun Xu <xuqunqun@live.cn>
2021-01-23 21:09:09 +0800

    Fix - the generated code within lost a namespace,

--- day02/B.aspx.designer.cs
+++ day02/B.aspx.designer.cs
@@ -7,34 +7,36 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+namespace HelloWebForms.day02
+{
+    public partial class B
+    {
 
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
-public partial class B {
+        /// <summary>
+        /// Alabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Alabel;
 
-    /// <summary>
-    /// form1 control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-/// <summary>
-/// Alabel control.
-/// </summary>
-/// <remarks>
-/// Auto-generated field.
-/// To modify move field declaration from designer file to code-behind file.
-/// </remarks>
-protected global::System.Web.UI.WebControls.Label Alabel;
-
-/// <summary>
-/// Adiv control.
-/// </summary>
-/// <remarks>
-/// Auto-generated field.
-/// To modify move field declaration from designer file to code-behind file.
-/// </remarks>
-protected global::System.Web.UI.HtmlControls.HtmlGenericControl Adiv;
+        /// <summary>
+        /// Adiv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Adiv;
+    }
 }
